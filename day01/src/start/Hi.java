2f04dbc635dd8241300eb1b4e2730d868f1f343c
junkyu92 <86291408+junkyu92@users.noreply.github.com>
2021-06-22 18:05:30 +0900

    Add files via upload

---
+++ day01/src/start/Hi.java
@@ -0,0 +1,11 @@
+package start;
+
+public class Hi {
+
+	public static void main(String[] args) {
+		System.out.println("덥다.");
+		System.out.println("dfsdfas");
+		System.out.println("dflks");
+	}
+
+}
